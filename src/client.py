645017e972f14581d2d981438b4405fcf3544f54
Michael <gpark608@gmail.com>
2021-02-21 20:28:18 -0700

    continuous conn

--- src/client.py
+++ src/client.py
@@ -2,10 +2,12 @@ import socket
 
 HOST = '127.0.0.1'
 PORT = 65412
+while True:
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client:
+        client.connect((HOST, PORT))
+        sentence = input('Input Message: ')
+        client.send(sentence.encode())
+        data = client.recv(1024)
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.connect((HOST, PORT))
-    s.sendall(b'Hello, world')
-    data = s.recv(1024)
-
-print('Received', repr(data))
+        print('Received: ', data.decode())
+        client.close()

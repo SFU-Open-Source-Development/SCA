58469a97cf3a537ba4091be6066682bf63a125cb
gpark608 <mpa72@sfu.ca>
2021-02-22 13:01:48 -0700

    Made the connection persistent

--- src/client.py
+++ src/client.py
@@ -1,13 +1,18 @@
 import socket
 
-HOST = '127.0.0.1'
+HOST = '192.168.1.70'
 PORT = 65412
+
+
 while True:
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client:
-        client.connect((HOST, PORT))
+    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client.connect((HOST, PORT))
+    print(client)
+    while True:
         sentence = input('Input Message: ')
         client.send(sentence.encode())
+        
         data = client.recv(1024)
 
         print('Received: ', data.decode())
-        client.close()
+        # client.close()
--- src/server.py
+++ src/server.py
@@ -2,28 +2,29 @@ import socket
 import sys
 
 
-HOST = '127.0.0.1'
+HOST = '192.168.1.70'
 PORT = 65412
 def main():
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         try:
             s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1) #https://stackoverflow.com/questions/6380057/python-binding-socket-address-already-in-use
-            s.bind(('', PORT))
+            s.bind((HOST, PORT))
         except:
             print("Error: Could not bind to port {}. Exiting!".format(PORT))
             sys.exit(1)
 
         print("The server is ready to receive")
         s.listen(1)
-
+        conn, addr = s.accept()
         while True:
-            conn, addr = s.accept()
+            
             print('Connected by', addr)
+            
             data = conn.recv(1024).decode()
             if not data:
                 break
             print("Received: {} from Client".format(data))
-            data = data + "123 from Server"
+            data = data.upper() + "123 from Server"
             conn.sendall(data.encode())
           
 
